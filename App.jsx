b952ae9daccfa09838b5c4bb8fdd189b0c035aa6
Lord Stoker <tpc00@iesemilidarder.com>
2024-12-15 22:26:03 +0100

    first commit

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import padsData from "./pads"
+import Pad from "./Pad"
+
+export default function App() {
+    const [pads, setPads] = React.useState(padsData)
+    
+    function toggle(id) {
+
+        setPads(prevPads =>
+         prevPads.map(pad =>
+          pad.id === id ? 
+          {...pad, on: !pad.on} : pad));
+
+    }
+    
+    const buttonElements = pads.map(pad => (
+        <Pad toggle={toggle} id={pad.id} key={pad.id} color={pad.color} on={pad.on}/>
+    ))
+    
+    return (
+        <main>
+            <div className="pad-container">
+                {buttonElements}
+            </div>
+        </main>
+    )
+}
---
+++ Pad.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Pad(props) {
+
+    
+    return (
+        <button 
+            style={{backgroundColor: props.color}}
+            className={props.on ? "on" : undefined}
+            onClick={() => props.toggle(props.id)}
+        ></button>
+    )
+}
